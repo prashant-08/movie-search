4c8bd3f6ef707b7a54dd2961af95effbe92415b5
prashant-08 <kumarprashant.iitg.6032@gmail.com>
2025-06-03 08:31:53 +0530

    Added navabr and basic styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
+import Navbar from './components/Navbar/Navbar'
 import MainRoutes from './routes/MainRoutes'
 
 function App() {
   // console.log(import.meta.env.VITE_API_KEY)
   return (
     <>
-      <nav>simple navbar</nav>
+      <Navbar />
       <MainRoutes />
     </>
   )
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import './Navbar.css'
+function Navbar () {
+    return (
+        <div className="navbar-wrapper">
+            <div>Movie Base</div>
+            <div className='search-bar'>
+                <input type='text' placeholder='Enter movie name' />
+            </div>
+            <div>Theme</div>
+        </div>
+    )
+}
+
+export default Navbar
