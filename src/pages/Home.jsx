cd99fd951bd055de39ce07fc38e371ddc882be36
prashant-08 <kumarprashant.iitg.6032@gmail.com>
2025-06-03 17:30:02 +0530

    added movie card component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,23 @@
+import MovieCard from "../components/MovieCard/MovieCard"
+import './Home.css'
+
 function Home () {
+    const movie = {
+      "Title": "Harry Potter and the Deathly Hallows: Part 2",
+      "Year": "2011",
+      "imdbID": "tt1201607",
+      "Type": "movie",
+      "Poster": "https://m.media-amazon.com/images/M/MV5BOTA1Mzc2N2ItZWRiNS00MjQzLTlmZDQtMjU0NmY1YWRkMGQ4XkEyXkFqcGc@._V1_SX300.jpg"
+    }
+
     return (
         <>
-        home page
+        <div className="movie-card-wrapper">
+            <MovieCard {...movie} />
+            <MovieCard {...movie} />
+            <MovieCard {...movie} />
+            <MovieCard {...movie} />
+        </div>
         </>
     )
 }
