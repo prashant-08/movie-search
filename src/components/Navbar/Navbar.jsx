b57e4b92f4059233e02a5c931db0c87341642ceb
prashant-08 <kumarprashant.iitg.6032@gmail.com>
2025-06-07 15:33:20 +0530

    Add: autocomplete feature

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,26 @@
+import { useState } from 'react'
 import './Navbar.css'
 function Navbar () {
+    const [isAutoCompleteVisible, setIsAutoCompleteVisible] = useState(false)
+
     return (
         <div className="navbar-wrapper">
             <div>Movie Base</div>
             <div className='search-bar'>
-                <input type='text' placeholder='Enter movie name' />
+                <input type='text' id='movie-search-input' placeholder='Enter movie name'
+                    onFocus={()=>{
+                        setIsAutoCompleteVisible(true)
+                    }}
+                    onBlur={()=>{
+                        setIsAutoCompleteVisible(false)
+                    }}
+                />
+                <div id='result-list' style={{display : (isAutoCompleteVisible) ? 'block' : 'none'}}>
+                        <div className='autocomplete-result'>result 1</div>
+                        <div className='autocomplete-result'>result 2</div>
+                        <div className='autocomplete-result'>result 3</div>
+                        <div className='autocomplete-result'>result 4</div>
+                </div>
             </div>
             <div>Theme</div>
         </div>
